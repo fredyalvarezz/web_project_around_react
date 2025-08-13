e0f05eb66102c3a26d964de3676cafea717d8d48
Fredy Alvarez <fredy-alexander1@hotmail.com>
2025-08-13 08:48:35 -0700

    actualizar app dentro de carpeta

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 
-import Header from "./components/Header/Header";
-import Main from "./components/Main/Main";
-import Footer from "./components/Footer/Footer";
+import Header from "./Header/Header";
+import Main from "./Main/Main";
+import Footer from "./Footer/Footer";
 
-import api from "./utils/api";
-import CurrentUserContext from "./contexts/CurrentUserContext";
+import api from "../utils/api";
+import CurrentUserContext from "../contexts/CurrentUserContext";
 
 function App() {
   const [currentUser, setCurrentUser] = useState(null);
